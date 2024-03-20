7e1dc4a3d0aa316a8ba9fd8598b10f9ece933185
Ovidiu <ovidiu.sbangu@gmail.com>
2024-03-20 18:14:51 +0200

    store users in dict

---
+++ python_learning_progress_tracker/student_management.py
@@ -0,0 +1,22 @@
+from python_learning_progress_tracker.student import Student
+
+
+class StudentManagement:
+
+    def __init__(self) -> None:
+        self.__students: dict[int, dict] = {}
+        self.__student_id = 1000
+
+    def add_student(self, student: "Student") -> None:
+        self.__students[self.__student_id] = {"student_data": student, "course_progress": []}
+        self.__student_id += 1
+
+    @property
+    def students(self):
+        return self.__students
+
+    def __len__(self) -> int:
+        return len(self.__students)
+
+    def __repr__(self) -> str:
+        return f"StudentManagement(students: {self.__students})"
---
+++ tests/test_student_management.py
@@ -0,0 +1,48 @@
+import pytest
+
+from python_learning_progress_tracker.student import Student
+from python_learning_progress_tracker.student_management import StudentManagement
+
+
+# student_manager = StudentManagement()
+
+
+@pytest.fixture()
+def student_manager():
+    return StudentManagement()
+
+
+def test_empty_students(student_manager):
+    assert len(student_manager) == 0
+
+
+def test_add_student(student_manager):
+    initial_len = len(student_manager.students)
+    students = [
+        ("Jean", "Clause", "jc@google.it"),
+        ("James", "Dean", "james.dean@gmail.com"),
+        ("Tim", "Bax", "tim.bax@gmail.com")
+    ]
+    for student in students:
+        student_manager.add_student(Student(*student))
+
+    new_len = len(student_manager.students)
+
+    assert new_len == initial_len + len(students)
+
+
+def test_increment_student_ids(student_manager):
+    initial_ids = set(student_manager.students.keys())
+
+    students = [
+        ("Jean", "Clause", "jc@google.it"),
+        ("James", "Dean", "james.dean@gmail.com"),
+        ("Tim", "Bax", "tim.bax@gmail.com")
+    ]
+
+    for student in students:
+        student_manager.add_student(Student(*student))
+
+    new_ids = set(student_manager.students.keys())
+
+    assert new_ids == initial_ids.union({1000, 1001, 1002})
